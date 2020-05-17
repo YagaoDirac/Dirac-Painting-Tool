0d4d92246f9ae9b9117486aafb408642dedfd737
YagaoDirac <yagaodirac@gmail.com>
2020-05-17 21:12:22 +0800

    Let's get started.

---
+++ convertion to gray scale/Convertion to gray scale.py
@@ -0,0 +1,46 @@
+import numpy as np
+from PIL import Image
+img = Image.open("in.png") #jpg, png, etc.
+pix = img.load()
+
+
+def sRGBtoLin(colorChannel):
+        #// Send this function a decimal sRGB gamma encoded color value
+        #// between 0.0 and 1.0, and it returns a linearized value.
+
+    if ( colorChannel <= 0.04045 ):
+        return colorChannel / 12.92
+    else:
+        return np.power((( colorChannel + 0.055)/1.055),2.4)
+
+#Y = (0.2126 * sRGBtoLin(vR) + 0.7152 * sRGBtoLin(vG) + 0.0722 * sRGBtoLin(vB))
+
+
+
+
+hight = img.size[0]
+Brightness = 0
+
+Max_Brightness = 0.2126 * sRGBtoLin(255) + 0.7152 * sRGBtoLin(255) + 0.0722 * sRGBtoLin(255)
+
+
+
+
+for x in range(img.size[0]):
+    print(str(int((x*100.)/hight))+"%")
+    for y in range(img.size[1]):
+        Brightness = 0.2126 * sRGBtoLin(pix[x,y][0]) + 0.7152 * sRGBtoLin(pix[x,y][1]) + 0.0722 * sRGBtoLin(pix[x,y][2])
+        Brightness =int(round(Brightness/Max_Brightness *255.0))
+        pix[x,y] = (Brightness,Brightness,Brightness,255)
+       
+
+
+img.save("Brightness output.png") 
+
+exit()
+
+
+
+
+
+
---
+++ saturation mask/Saturatino mask gen for png file.py
@@ -0,0 +1,45 @@
+# import os
+# from os import walk
+
+# f = []
+# for (dirpath, dirnames, filenames) in walk(os.getcwd()):
+#     print(filenames)
+#     #f.extend(filenames)
+#     break
+
+# exit(0)
+
+
+import numpy as np
+from PIL import Image
+img = Image.open("in.png") #jpg, png, etc.
+pix = img.load()
+
+
+max = 0.
+min = 0.
+ratio =0. 
+hight = img.size[0]
+
+for x in range(img.size[0]):
+    print(str(int((x*100.)/hight))+"%")
+    for y in range(img.size[1]):
+        max = np.max(pix[x,y][0:3])
+        min = np.min(pix[x,y][0:3])
+        if 0==max:
+            ratio = 0
+        else:
+            ratio = int(255*(min/max))
+        pix[x,y] = (ratio,ratio,ratio,255)
+       
+
+
+img.save("saturation mask output.png") 
+
+exit()
+
+
+
+
+
+
